848874e843ff5e154eb61df2ab56cbca4d533c43
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-27 19:58:35 +0300

    create generateMEssage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http=require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+
+const {generateMessage} = require("./utils/message");
 const publicPath=path.join(__dirname,"../public");
 const port=process.env.PORT || 3000;
 var app=express();
@@ -18,26 +20,17 @@ io.on("connection",(socket) => {
     //socket.emit from Admin text welcome to chat app
 
 
-    socket.emit("newMessage",{
-       from:"Admin",
-        text:"Welcome to  chat app",
-        createdAt:new Date().getTime()
-    });
+    socket.emit("newMessage",generateMessage("Admin","Welcome to the chat app"));
+
 
     // socket.broadcast.emit from Admin text New user joined
-    socket.broadcast.emit("newMessage",{
-        from:"Admin",
-        text:"New user joined the channel",
-        createdAt:new Date().getTime()
-    });
+    socket.broadcast.emit("newMessage",generateMessage("Admin","New user joined"));
 
     socket.on("createMessage",(message) => {
        console.log("createMessage",message);
-       io.emit("newMessage",{ // her baglantıyı emit eder
-           from:message.from,
-           text:message.text,
-           createdAt:new Date().getTime()
-       });
+       io.emit("newMessage",generateMessage(message.from,message.text))// her baglantıyı emit eder
+
+
 
 
     //Broadcast yapıyorum
