ce942b16105c9276d10a409051e2daa7d845a023
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-28 00:17:10 +0300

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,16 @@
         jQuery("#messages").append(li);
     });
 
+    //for geolocation
+    socket.on("newLocationMessage",function(message){
+        var li=jQuery("<li></li>");
+        var a=jQuery("<a target='_blank'>My current location</a>");
+
+        li.text(message.from+": ");
+        a.attr("href",message.url);
+        li.append(a);
+        jQuery("#messages").append(li);
+    });
     //acknowledgement yapıyorum otomatik mesaj bu
    /* socket.emit("createMessage",{
         from:"Murat",
@@ -35,4 +45,21 @@
         },function(){
 
         });
+    });
+
+    var locationButton = jQuery("#send-location");
+
+    locationButton.on('click',function (){
+       if(!navigator.geolocation){
+           return alert("Geolocation not supported by your browser.")
+       }
+
+       navigator.geolocation.getCurrentPosition(function (position){
+        socket.emit("createLocationMessage",{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        });
+       },function () {
+          alert("Unable to fetch location");
+       });
     });
